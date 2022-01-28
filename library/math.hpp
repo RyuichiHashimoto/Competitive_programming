fb290566c6de0301a73da2d2beb7a2baf35e910a
RyuichiHashimoto <ryuichi.hashimoto.git@gmail.com>
2022-01-29 00:41:16 +0900

    merge some files

--- library/math.hpp
+++ library/math.hpp
@@ -1,6 +1,46 @@
 #include <iostream>
-#include <bits/stdc++.h>
 using namespace std;
+#include <bits/stdc++.h>
+#include <limits.h>
+#include <queue>
+
+typedef long long ll;
+
+ll mod_pow(ll x, ll expo, ll mod)
+{
+    /*
+        return (x  ** expor ) % mod
+    */
+
+    ll res = 1;
+
+    while (expo > 0)
+    {
+        if (expo & 1)
+        {
+            res = (res * x) % mod;
+        }
+        x = (x * x) % mod;
+
+        expo = expo >> 1;
+    }
+    return res;
+}
+
+ll pow(ll x , ll n){
+    
+    
+    ll ret = 1;
+    while (n> 0){
+        if (n &1 == 1){
+            ret = (ret * x) ;
+        }
+        x = (x * x);
+
+        n = n >> 1;        
+    }
+    return ret;
+}
 
 namespace prime_number
 {
@@ -94,10 +134,9 @@ namespace prime_number
     }
 }
 
-int main(){
 
-    long long s = 43;
-
-    cout << prime_number::is_prime(s);
-
+int main(void)
+{
+    cout << pow(10, 5);
 }
+
