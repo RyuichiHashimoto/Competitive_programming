41b3f30a37623fd0ce3882d18550ba8ca55ffb3f
RyuichiHashimoto <ryuichi.hashimoto.git@gmail.com>
2022-01-29 16:16:44 +0900

    solve kenkei 021 problem

--- atcoder_kakomon/kenkei_90/021_Come_back_in_One_Piece.cpp
+++ atcoder_kakomon/kenkei_90/021_Come_back_in_One_Piece.cpp
@@ -24,7 +24,7 @@ public:
 
     edge(int _from, int _to, int _cap, int _cost, int _rev)
     {
-        
+  
         from = _from;
         to = _to;
         cap = _cap;
@@ -34,7 +34,7 @@ public:
 };
 
 class Graph
-{
+{    
 public:
     vector<vector<edge>> G;
     int minimum_node_idx;
@@ -66,28 +66,116 @@ public:
 
 namespace SCC {
 
-}
+    void add_edge(Graph &, int, int);
+    void post_order_by_dfs(Graph &, vector<int> &, vector<bool> &, int);
+    void __create_inverse_graph(Graph &, Graph &);
+
+    void add_edge(Graph &graph, int from, int to)
+    {
+        edge e(from, to, 0, 0, 0);
+        graph.add_edge(e);
+    }
+
+
+    /*
+
+    */
+    void __create_edge_inverse_graph(Graph &original, Graph &inverse){
+        inverse = Graph(original.n_vetrics);
+
+        for (int i = 1; i <= original.n_vetrics;i++){
+            for (auto e : original.G[i]){
+                edge inv_e(e.to, e.from, e.cap, e.cost, e.rev);
+                inverse.add_edge(inv_e);
+            }
+        }
+    }
+
+    /*
+    与えられた有効グラフを復数の強連結成分に分解する。
+    帰り値 [vector<int>, vector<int>, ..., vector<int>]
+    各vector<int>には、同じ教連結成分に属するノード番号が代入されている。
+    */
+    vector<vector<int>> SCC(Graph &graph){
+        
+        Graph inverse_graph(graph.n_vetrics);
+
+        //アルゴリズムに強連結成分が必要なので、事前に作成
+        __create_edge_inverse_graph(graph, inverse_graph);
+
+        vector<bool> used(graph.n_vetrics + 1);
+        fill(used.begin(), used.end(), false);
+
+
+        //帰りがけ順に並び替えてノード番号を入れる。
+        vector<int> post_order;
+        for (int i = 1; i <= graph.n_vetrics; i++)
+        {
+            if (!used[i])
+                __post_order_by_dfs(graph, post_order, used, i);
+        }
+        
+        reverse(post_order.begin(), post_order.end());
+
+        fill(used.begin(), used.end(), false);
+        vector<vector<int>> ret;
+        for (auto i: post_order){
+            if (!used[i]){
+                vector<int> inv_post_order;
+                __post_order_by_dfs(inverse_graph, inv_post_order, used, i);
+                ret.push_back(inv_post_order);
+                for(auto s: inv_post_order){
+                    // cout << s << " ";
+                }
+                // cout << endl;
+            }
+        }
+        return ret;
+    }
+
+    /*
+        帰りがけ順で各ノードに番号をつける。
+        引数のpost_order(vector<int>) に帰りがけの順番でノード番号を入れていく。
+        つまり、post_order = [(帰りがけ順で一番目のノード), (帰りがけ順で2番目のノード), ..., (帰りがけ順でn番目のノード)]
+        
+        target_point: ノード番号、[1, graph.n_vetrics]の範囲で指定
+    */
+    void __post_order_by_dfs(Graph &graph, vector<int> &post_order, vector<bool> &used,int target_point)
+    {
+        used[target_point] = true;
+    
+        for (auto e : graph.G[target_point])
+        {
+            if (!used[e.to]){
+                __post_order_by_dfs(graph, post_order, used, e.to);
+            }
+        }
+        post_order.push_back(target_point);
+    }
+
+};
 
 int main(void)
 {
     int N,M;
     cin >> N >> M;
 
-    Graph graph(N+1);
+    Graph graph(N);
     rep(i, M)
     {
-        int a,b,c;
-        cin >> a >> b >> c;
-        Dykstra::add_edge(graph, a, b, c);
-        Dykstra::add_edge(graph, b, a, c);
+        int tmp1, tmp2;
+        cin >> tmp1 >> tmp2;
+        SCC::add_edge(graph, tmp1,tmp2);        
     }
-    vector<ll> fromA = Dykstra::Dykstra(graph, 1);
-    vector<ll> fromB = Dykstra::Dykstra(graph, N);
-    cout << fromA[N]<<endl;
-    rep(i, N - 2)
-    {
-        cout << fromA[i + 2] + fromB[i+2]<<endl;
+
+    vector<vector<int>> ret = SCC::SCC(graph);
+
+    ll ret_val = 0;
+    for (vector<int> each_group : ret)
+    {        
+        ll size = (ll)each_group.size();
+        ret_val = ret_val + (size * (size - 1ll)) / 2ll;
     }
-    cout << fromA[N]<<endl;
+    cout <<ret_val;
 }
      
