e17d65006a405b7a8d4ca642081bfc1baba23483
RyuichiHashimoto <ryuichi.hashimoto.git@gmail.com>
2022-01-29 17:51:50 +0900

    can exe multiple files

--- exe.sh
+++ exe.sh
@@ -16,11 +16,7 @@ if test $# -eq 1 || test $# -eq 2;then
     source_file=$1    
     txt_file=`echo ${source_file} | rev | cut -c 4- | rev`txt
     
-
-
     echo we compile $1 file
-
-
     g++ -o ${outputexe} ${source_file}
     RESULT_SIGNAL=$?
     
@@ -28,17 +24,31 @@ if test $# -eq 1 || test $# -eq 2;then
         echo "コンパイルに成功しました。"
         
         if test $# -eq 1; then
-            if [ -e ${txt_file} ]; then
+            find_flag=0
+            for i in `seq 1 10`
+            do
+                txt_file=`echo ${source_file} | rev | cut -c 5- | rev`_${i}.txt
+                if [ -e ${txt_file} ]; then
+                    find_flag=1;
+                    echo ----------------------
+                    echo ${txt_file} is found
+                    echo ----------------------
+                    ./${outputexe} < ${txt_file}2
+                    echo 
+                fi
+            done
+            txt_file=`echo ${source_file} | rev | cut -c 4- | rev`txt                
+            if [ -e ${txt_file} ]; then                
+                find_flag=1;
                 echo ----------------------
                 echo ${txt_file} is found
                 echo ----------------------
                 ./${outputexe} < ${txt_file}
-            else
-                ./${outputexe}
             fi
 
-                                            
-            
+            if test ${find_flag} -eq 0; then
+                ./${outputexe}
+            fi                                                 
         elif test $# -eq 2; then
             ./${outputexe} < $2
         fi
