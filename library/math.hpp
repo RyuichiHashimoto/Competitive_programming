1b67fa096510d62d72a272240a2c70e670517acb
RyuichiHashimoto <ryuichi.hashimoto.git@gmail.com>
2022-03-24 00:59:02 +0900

    fix bug in prime_factor_as_vect

--- library/math.hpp
+++ library/math.hpp
@@ -2,22 +2,20 @@
 using namespace std;
 #include <bits/stdc++.h>
 #include <limits.h>
+
 #include <queue>
 
 typedef long long ll;
 
-ll mod_pow(ll x, ll expo, ll mod)
-{
+ll mod_pow(ll x, ll expo, ll mod) {
     /*
         return (x  ** expor ) % mod
     */
 
     ll res = 1;
 
-    while (expo > 0)
-    {
-        if (expo & 1)
-        {
+    while (expo > 0) {
+        if (expo & 1) {
             res = (res * x) % mod;
         }
         x = (x * x) % mod;
@@ -27,30 +25,28 @@ ll mod_pow(ll x, ll expo, ll mod)
     return res;
 }
 
-ll pow(ll x , ll n){
-    
-    
+ll pow(ll x, ll n) {
+
+
     ll ret = 1;
-    while (n> 0){
-        if (n &1 == 1){
-            ret = (ret * x) ;
+    while (n > 0) {
+        if (n & 1 == 1) {
+            ret = (ret * x);
         }
         x = (x * x);
 
-        n = n >> 1;        
+        n = n >> 1;
     }
     return ret;
 }
 
-namespace prime_number
-{
+namespace prime_number {
 
     typedef long long ll;
     typedef pair<long, long> P;
     const int MAX_N = 400000;
 
-    bool is_prime(ll number)
-    {
+    bool is_prime(ll number) {
         /*
             numberが素数か判定する。
 
@@ -59,31 +55,25 @@ namespace prime_number
             False: 引数 numberが素数ではない
             計算量: sqrt(number)
         */
-        for (ll i = 2; i * i <= number; i++)
-        {
-            if (number % i == 0)
-                return false;
+        for (ll i = 2; i * i <= number; i++) {
+            if (number % i == 0) return false;
         }
         return number != 1;
     }
 
-    vector<ll> divisor(ll number)
-    {
+    vector<ll> divisor(ll number) {
         /*
-            数字 numberの約数を小さい順に列挙                    
+            数字 numberの約数を小さい順に列挙
             計算量: sqrt(number);
 
         */
         vector<ll> res;
 
-        for (ll i = 1; i * i <= number; i++)
-        {
+        for (ll i = 1; i * i <= number; i++) {
 
-            if (number % i == 0)
-            {
+            if (number % i == 0) {
                 res.push_back(i);
-                if (i * i != number)
-                {
+                if (i * i != number) {
                     res.push_back(number / i);
                 }
             }
@@ -91,44 +81,37 @@ namespace prime_number
         return res;
     }
 
-    vector<P> prime_factor_as_vect(ll number)
-    {
+    vector<P> prime_factor_as_vect(ll number) {
+        // 素因数分解
+
 
         vector<P> ret;
-        for (ll i = 2; i * i < number; i++)
-        {
+        for (ll i = 2; i * i <= number; i++) {
             ll count = 0;
-            while (number % i == 0)
-            {
+            while (number % i == 0) {
                 count++;
                 number = number / i;
             }
-            if (count != 0)
-                ret.push_back(P(i, count));
+            if (count != 0) ret.push_back(P(i, count));
         }
-        if (number != 1)
-            ret.push_back(P(number, 1));
+        if (number != 1) ret.push_back(P(number, 1));
 
         return ret;
     }
 
-    map<ll, ll> prime_factor_as_map(ll number)
-    {
+    map<ll, ll> prime_factor_as_map(ll number) {
         map<ll, ll> ret;
 
-        for (ll i = 2; i * i < number; i++)
-        {
+        for (ll i = 2; i * i <= number; i++) {
             ll count = 0;
-            while (number % i == 0)
-            {
+            while (number % i == 0) {
                 count++;
                 number = number / i;
             }
             ret[i] = count;
         }
 
-        if (number != 1)
-            ret[number] = 1;
+        if (number != 1) ret[number] = 1;
 
         return ret;
     }
@@ -136,21 +119,17 @@ namespace prime_number
     /*
        N以下の素数を列挙する。
     */
-    vector<int> Eratosthenes(int N)
-    {
+    vector<int> Eratosthenes(int N) {
         vector<int> prime_list;
 
-        bool *is_prime = new bool[N+1];
+        bool *is_prime = new bool[N + 1];
         fill(is_prime, is_prime + N, true);
 
-        for (int i = 2; i <= N; i++)
-        {
-            if (is_prime[i])
-            {
+        for (int i = 2; i <= N; i++) {
+            if (is_prime[i]) {
                 prime_list.push_back(i);
 
-                for (int j = 1; j * i <= N; j++)
-                {
+                for (int j = 1; j * i <= N; j++) {
                     is_prime[i * j] = false;
                 }
             }
@@ -158,11 +137,9 @@ namespace prime_number
         delete is_prime;
         return prime_list;
     }
-}
+}  // namespace prime_number
 
 
-int main(void)
-{
+int main(void) {
     cout << pow(10, 5);
 }
-
