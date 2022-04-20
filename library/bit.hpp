4c4f264f2b21dbd13013bd48c157f6eb1c751c07
RyuichiHashimoto <ryuichi.hashimoto.git@gmail.com>
2022-04-20 23:29:12 +0900

    display(long long ) to display(T)

--- library/bit.hpp
+++ library/bit.hpp
@@ -1,21 +1,22 @@
-#include <iostream>
 #include <bits/stdc++.h>
+
+#include <iostream>
 using namespace std;
 
 
-
-void display(ll x){
+template <typename T>
+void display(T x) {
 
     int bit[64];
-    fill(bit,bit+64,0);
+    fill(bit, bit + 64, 0);
 
     int counter = 0;
-    while(x != 0){
+    while (x != 0) {
         bit[counter++] = (int)(x & 1);
         x = x >> 1;
     }
-    reverse(bit,bit+64);
-    for(int i=0;i<64;i++){
+    reverse(bit, bit + 64);
+    for (int i = 0; i < 64; i++) {
         cout << bit[i];
     }
     cout << endl;
