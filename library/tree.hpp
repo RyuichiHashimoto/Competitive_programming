9fcc5488e41a4806a72cd09d7c0f98a2981d699a
ubuntu <hashimoto>
2022-12-04 00:33:48 +0900

    erase useless union find implementation

--- library/tree.hpp
+++ library/tree.hpp
@@ -230,57 +230,6 @@ namespace tree {
 };  // namespace tree
 
 
-class Union_Find_union {
-  private:
-    vector<int> par;
-    vector<int> rank;
-
-  public:
-    Union_Find(int size) {
-        par = new int[size];
-        rank = new int[size];
-
-        for (int i = 0; i < size; i++) {
-            par[i] = i;
-            rank[i] = 0;
-        }
-    }
-
-    ~Union_Find() {
-        delete par;
-        delete rank;
-    }
-
-    int root(int x) {
-        if (par[x] == x) {
-            return x;
-        } else {
-            par[x] = root(par[x]);
-            return par[x];
-        }
-    }
-
-    void unite(int x, int y) {
-        x = root(x);
-        y = root(y);
-
-        if (x == y) return;
-
-        if (rank[x] < rank[y]) {
-            par[x] = y;
-        } else {
-            par[y] = x;
-            if (rank[x] == rank[y]) {
-                rank[x]++;
-            }
-        }
-    }
-
-    bool same(int x, int y) {
-        return root(x) == root(y);
-    }
-};
-
 
 class Union_Find {
   private:
